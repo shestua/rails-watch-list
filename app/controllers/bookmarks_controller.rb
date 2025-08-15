0b746c4d47fc5542c22bbe0d57ed6afeda61fed1
Aleksei Shestua <shestuaa@yahoo.com>
2025-08-15 11:08:48 +0200

    all saves

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,6 +15,12 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(List.find(params[:list_id]))
+  end
+
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
   end
